eff34117ba9ac485fa5e01bac89245b3bf8cc919
Ricardo <rikisj5649@gmail.com>
2024-03-04 09:54:10 -0600

    Proyectos de back y front end

---
+++ Zorriana-Back/src/main/java/com/zorriana/backend/ZorrianaBackApplication.java
@@ -0,0 +1,13 @@
+package com.zorriana.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZorrianaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZorrianaBackApplication.class, args);
+	}
+
+}
