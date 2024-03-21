b3cc30563d757989dd3d9dde9028d39b7473e9e6
JjersaR <86206596+jjersar@users.noreply.github.com>
2024-03-21 12:30:39 -0600

    Agregando funcionalidad a las demas tablas y agregando carpeta dto y mapper

--- Zorriana-Back/src/main/java/com/zorriana/backend/ventaArticulos/service/impl/VentaArticulosServiceImpl.java
+++ Zorriana-Back/src/main/java/com/zorriana/backend/ventaArticulos/service/impl/VentaArticulosServiceImpl.java
@@ -1,13 +1,22 @@
 package com.zorriana.backend.ventaArticulos.service.impl;
 
+import com.zorriana.backend.ventaArticulos.entity.VentaArticulos;
 import com.zorriana.backend.ventaArticulos.repository.IVentaArticulosRepository;
 import com.zorriana.backend.ventaArticulos.service.IVentaArticulosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VentaArticulosServiceImpl implements IVentaArticulosService {
 
   @Autowired
   private IVentaArticulosRepository repository;
+
+  @Override
+  public List<VentaArticulos> findByIdUsuario(Long idUsuario) {
+    return repository.findByUsuarioID(idUsuario);
+  }
+
 }
