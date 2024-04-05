f906f504fbfe916c58e3bdfab1d66a1ba1507633
JjersaR <rikisj5649@gmail.com>
2024-04-05 17:02:33 -0600

    Listando los productos desde React con axios

--- zorriana-front/src/App.js
+++ zorriana-front/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import ListarProductos from './componentes/ListarProductos';
 
 function App() {
   return (
     <div className="App">
+    <ListarProductos/>
     </div>
   );
 }
